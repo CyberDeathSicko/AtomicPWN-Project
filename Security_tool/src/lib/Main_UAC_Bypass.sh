fe19dc02b64d5feed9447fc7808f11765ecf1575
CyberDeathSicko <145595167+CyberDeathSicko@users.noreply.github.com>
2023-12-05 19:38:16 -0500

    Update Main_UAC_Bypass.sh

--- Security_tool/src/lib/Main_UAC_Bypass.sh
+++ Security_tool/src/lib/Main_UAC_Bypass.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-# Global variable for sudoers file
-SUDOERS_FILE=""
-
-# Function to create a sudoers file with UAC bypass
 create_sudoers_file() {
     local temp_dir=$(mktemp -d)
-    SUDOERS_FILE="$temp_dir/uac_bypass"
+    local sudoers_file="$temp_dir/uac_bypass"
 
-    # Add a line to the sudoers file allowing user access without password
-    echo "$USER ALL=(ALL) NOPASSWD:ALL" > "$SUDOERS_FILE"
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" > "$sudoers_file"
 
-    # Install the sudoers file with the appropriate permissions
-    sudo install -m 440 -o root -g root "$SUDOERS_FILE" "/etc/sudoers.d/uac_bypass"
+    sudo install -m 440 -o root -g root "$sudoers_file" "/etc/sudoers.d/uac_bypass"
 
-    echo "$SUDOERS_FILE"
+    echo "$sudoers_file"
 }
 
-# Function to clean up temporary files
 cleanup() {
     sudo rm -f "/etc/sudoers.d/uac_bypass"
-    [ -n "$SUDOERS_FILE" ] && rm -rf "$SUDOERS_FILE"
 }
 
-# Source the UAC Bypass script
-source lib/uac_bypass.sh
+# Execute UAC Bypass
+execute_uac_bypass() {
+    local temp_sudoers_file
+    temp_sudoers_file=$(create_sudoers_file)
+    echo -e "\nUAC Bypass Completed."
+    cleanup
+}
 
-# Call the execute_uac_bypass function
+# Execute UAC Bypass
 execute_uac_bypass
